32c2ea826bdc2e33d06d27b2c1081ca5d4374124
Akshay <akshayyadav7613@gmail.com>
2019-02-11 22:29:02 +0530

    finish the app.....

--- lib/main.dart
+++ lib/main.dart
@@ -57,7 +57,9 @@ class HomePageState extends State<HomePage> {
     return Scaffold(
 
       appBar: AppBar(title: Text("poke app"),),
-      body: pokeHub == null ? Center(child: CircularProgressIndicator(),): GridView.count(crossAxisCount: 2,children: pokeHub.pokemon.map((Pokemon poke) => Padding(padding: const EdgeInsets.all(2.0),  child: InkWell(
+      body: pokeHub == null ? Center(child: CircularProgressIndicator(),):
+      GridView.count(crossAxisCount: 3,children: pokeHub.pokemon.map((Pokemon poke) =>
+          Padding(padding: const EdgeInsets.all(2.0),  child: InkWell(
         onTap: (){
         Navigator.push(context, MaterialPageRoute(builder: (context) => PokeDetail(
           pokemon: poke,
@@ -70,18 +72,19 @@ class HomePageState extends State<HomePage> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
                 Container(
-                  height: 100.0,
-                  width:  100.0,
+                  height: 80.0,
+                  width:  80.0,
                   decoration: BoxDecoration(
                     image: DecorationImage(image: NetworkImage(poke.img))),),
-                Text(poke.name,style: TextStyle(fontSize: 20.0,fontWeight: FontWeight.bold,),)
+                Text(poke.name,style: TextStyle(fontSize: 12.0,fontWeight: FontWeight.bold,),)
               ],
             ),
           ),
         ),
       ),)).toList() ,),
-      drawer: Drawer(),
-      floatingActionButton: FloatingActionButton(onPressed: (){} ,child: Icon(Icons.ac_unit),),
+
+      //drawer: Drawer(),
+      //floatingActionButton: FloatingActionButton(onPressed: (){} ,child: Icon(Icons.ac_unit),),
 
     );
   }
--- lib/pokemondeatils.dart
+++ lib/pokemondeatils.dart
@@ -7,31 +7,45 @@ class PokeDetail extends StatelessWidget {
 
   PokeDetail({this.pokemon});
 
-bodyWiget() => Stack(
-  children: <Widget>[
-    Container(
-      child: Card(
-        child: Column(
-          children: <Widget>[
-            Text(pokemon.name,style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
-            Text("hight: ${pokemon.height}"),
-            Text("weight: ${pokemon.weight}"),
-            Text("type: ${pokemon.type}"),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: pokemon.type.map((t)=>FilterChip(label: Text(t),backgroundColor: Colors.green, onSelected: (b){},)).toList(),
-            ),
-            Text("weakness"),
+    bodyWiget(BuildContext context) => Stack(children: <Widget>[
+    Positioned(
+      height: MediaQuery.of(context).size.height /1.5,
+      width: MediaQuery.of(context).size.width -20,left: 10.0,
+      top:  MediaQuery.of(context).size.height *0.1,
 
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: pokemon.weaknesses.map((t)=>FilterChip(label: Text(t),backgroundColor: Colors.green, onSelected: (b){},)).toList(),
-            ),
+      child: Container(
+        child: Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+          child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: <Widget>[
+              SizedBox(height: 20,),
+              Text(pokemon.name,style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),),
+              Text("hight: ${pokemon.height}"),
+              Text("weight: ${pokemon.weight}"),
 
-          ],
+              Text("type"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: pokemon.type.map((t)=>FilterChip(label: Text(t),backgroundColor: Colors.green, onSelected: (b){},)).toList(),
+              ),
+
+              Text("weakness"),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: pokemon.weaknesses.map((t)=>FilterChip(label: Text(t),backgroundColor: Colors.green, onSelected: (b){},)).toList(),
+              ),
+
+            ],
+          ),
         ),
       ),
-    )
+    ),
+
+    Align(
+      alignment: Alignment.topCenter,
+      child:  Hero(tag:  pokemon.img, child: Container(height: 120.0, width: 120.0,
+      decoration: BoxDecoration(image: DecorationImage(image: NetworkImage(pokemon.img),fit:
+      BoxFit.cover)),),),
+    ),
   ],
 );
 
@@ -42,7 +56,7 @@ bodyWiget() => Stack(
     return Scaffold(
       backgroundColor: Colors.cyan,
       appBar: AppBar(backgroundColor: Colors.cyan, elevation:  0.0, title: Text(pokemon.name),),
-      body: bodyWiget(),
+      body: bodyWiget(context),
       
     );
 
